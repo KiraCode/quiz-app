0b88bdde9de7ad386b89e541c85bf0d5c758fba8
tejas git <d9489079@gmail.com>
2025-11-28 21:25:52 +0530

    node modules been added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
@@ -42,4 +43,41 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = {register}
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and Password required to login" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: `No user exists with ${email} email id` });
+    }
+
+    // check for the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    // generate jwt token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_TOKEN,
+      { expiresIn: "300m" }
+    );
+
+    res.status(200).json({ accessToken });
+  } catch (error) {
+    console.error("Error logging in", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { register, login };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./utils/db.js");
+const userRouter = require("./routes/userRoutes.js");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // express middleware
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // db connection
 db();
+
+// routes middleware
+app.use("/api/v1/users", userRouter);
 
 // connect server
 app.listen(PORT, () => {
