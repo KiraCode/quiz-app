7ffb20c1d1605f5a2ad9e1a8c6a0a387311199a5
tejas git <d9489079@gmail.com>
2025-12-03 22:06:01 +0530

    authslice been added

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { signAPI } from "../thunk/authThunk";
+import { loginAPI, logoutUser, signupAPI } from "../thunks/authThunks";
 
 const initialState = {
   isAuthenticated: Boolean(localStorage.getItem("accessToken")?.trim()),
@@ -13,21 +13,44 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   extraReducers(builder) {
-    builder.addCase(signAPI.pending, (state) => {
-      (state.isAuthenticated = false),
-        (state.email = null),
-        (state.error = null),
-        (satisfies.loading = true);
+    builder.addCase(signupAPI.pending, (state) => {
+      state.isAuthenticated = false;
+      state.email = null;
+      state.error = null;
+      state.loading = true;
     });
-    builder.addCase(signAPI.fulfilled, (state, action) => {
+    builder.addCase(signupAPI.fulfilled, (state, action) => {
       const { user } = action.payload;
       state.loading = false;
       state.email = user.email;
     });
-    builder.addCase(signAPI.rejected, (state, action) => {
-      const errorMesssage = action.payload;
+    builder.addCase(signupAPI.rejected, (state, action) => {
+      const errorMessage = action.payload;
       state.loading = false;
-      state.error = errorMesssage;
+      state.error = errorMessage;
+    });
+    builder.addCase(loginAPI.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+      state.isAuthenticated = false;
+      state.email = "";
+      state.isTokenExpired = false;
+    });
+    builder.addCase(loginAPI.fulfilled, (state, action) => {
+      const email = action.payload;
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.email = email;
+    });
+    builder.addCase(loginAPI.rejected, (state, action) => {
+      const errorMessage = action.payload;
+      state.loading = false;
+      state.error = errorMessage;
+    });
+    builder.addCase(logoutUser.fulfilled, (state, action) => {
+      state.isAuthenticated = false;
+      state.email = null;
+      state.isTokenExpired = action.payload;
     });
   },
 });
